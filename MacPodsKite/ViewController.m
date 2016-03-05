917db6f7d626149c595727284eed99277aef453a
skeyboy <lylapp@163.com>
2016-03-05 19:10:09 +0800

    Initial Commit

---
+++ MacPodsKite/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MacPodsKite
+//
+//  Created by 李雨龙 on 16/3/5.
+//  Copyright © 2016年 杰之能. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MacPodsKite/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MacPodsKite
+//
+//  Created by 李雨龙 on 16/3/5.
+//  Copyright © 2016年 杰之能. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
