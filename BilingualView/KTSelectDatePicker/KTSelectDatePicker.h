36d8d9e337e9447dbd5c494af532a99d3f601638
skeyboy <lylapp@163.com>
2016-03-07 10:56:50 +0800

    日期控件

---
+++ BilingualView/KTSelectDatePicker/KTSelectDatePicker.h
@@ -0,0 +1,26 @@
+//
+//  KTSelectDatePicker.h
+//  KTSelectDatePicker
+//
+//  Created by hcl on 15/10/9.
+//  Copyright (c) 2015年 hcl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KTSelectPickerView.h"
+
+typedef void (^DataTimeSelect)(NSDate *selectDataTime);
+
+@interface KTSelectDatePicker : NSObject
+@property (strong, nonatomic) KTSelectPickerView *pickerView;
+
+///是否可选择当前时间之前的时间,默认为NO
+@property (nonatomic,assign) BOOL isBeforeTime;
+
+///DatePickerMode,默认是DateAndTime
+@property (assign, nonatomic) UIDatePickerMode datePickerMode;
+
+- (void)didFinishSelectedDate:(DataTimeSelect)selectDataTime;
+- (void)showDatePickerView;
+- (void)dismissDatePickerView;
+@end
